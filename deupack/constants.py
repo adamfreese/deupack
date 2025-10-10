40e5a5e76bb4c43d6e84ada2884b3fc330da1e60
Adam Freese <adamfreese@proton.me>
2025-10-10 16:26:07 -0400

    Update list of constants

--- deupack/constants.py
+++ deupack/constants.py
@@ -1,11 +1,24 @@
 import numpy as np
 
 # Basicl physical constants
-hbar  = 0.197326968 # GeV-fm
+hbar     = 0.197326968     # GeV-fm
+alphaQED = 0.0072973525643 # fine structure constant
 
 # Masses
-Md    = 1.87561294257 # deuteron mass (GeV)
-mN    = 0.9389186795  # averaged nucleon mass (GeV)
+mpi_p = 0.13957039    # charged pion mass (GeV)
+mpi_0 = 0.1349768     # neutral pion mass (GeV)
+mpi   = 0.13803919    # average pion mass (GeV)
+mp    = 0.93827208943 # proton mass (GeV)
+mn    = 0.93956542194 # neutron mass (GeV)
+mN    = 0.93891875569 # averaged nucleon mass [arithmetic mean] (GeV)
+mr    = 0.46945915515 # reduced proton-nuetron mass [half harmonic mean] (GeV)
+Md    = 1.87561294200 # deuteron mass (GeV)
 
-# Misc constants
-kappa = np.sqrt((2*mN-Md)*mN) / hbar # asymptotic decay length, fm**-1
+# Electromagnetic properties
+mu_p  =  2.79284734463 # proton magnetic moment (nuclear magnetons)
+mu_n  = -1.91304276    # neutron magnetic moment (nuclear magnetons)
+mu_d  =  0.8574382335  # deuteron magnetic moment (nuclear magnetons)
+
+# Misc derived constants
+Ed    = mp + mn - Md                 # deuteron binding energy (GeV)
+kappa = np.sqrt((2*mN-Md)*mN) / hbar # asymptotic decay length (fm**-1)
