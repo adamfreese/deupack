fadedf72e51354edfe58f85b87ebe3c5c89901a6
Adam Freese <adamfreese@proton.me>
2025-11-10 15:03:04 -0500

    include data from Panteleeva et al

--- workspace/plots.py
+++ workspace/plots.py
@@ -9,92 +9,78 @@ mpl.rc('font',size=26,family='cmr10',weight='normal')
 mpl.rc('text',usetex=True)
 mpl.rc('text.latex', preamble=r"\usepackage{bm,amsmath,amssymb,amsfonts,mathrsfs}")
 
-def plot_one_mff(name, logscale=False):
-    wimdf = dp.mff.wim.make_wimffs()
-    hzdf = dp.mff.hz.make_hzmffs()
-    Delta2 = wimdf['Delta2']
+def select_mff(name, dl2):
     if(name=='AU'):
-        F_our = dp.mff.AU(np.sqrt(Delta2))
+        F = dp.mff.AU(np.sqrt(dl2))
     elif(name=='AT'):
-        F_our = dp.mff.AT(np.sqrt(Delta2))
+        F = dp.mff.AT(np.sqrt(dl2))
     elif(name=='J'):
-        F_our = dp.mff.J(np.sqrt(Delta2))
+        F = dp.mff.J(np.sqrt(dl2))
     elif(name=='DU'):
-        #F_our = dp.mff.DU(np.sqrt(Delta2))#, DN=dp.mff.nucleon.DN_fc)
-        F_our = dp.mff.DU(np.sqrt(Delta2), DN=dp.mff.nucleon.DN_hz)
+        #F = dp.mff.DU(np.sqrt(dl2))#, DN=dp.mff.nucleon.DN_fc)
+        F = dp.mff.DU(np.sqrt(dl2), DN=dp.mff.nucleon.DN_hz)
     elif(name=='DT1'):
-        #F_our = dp.mff.DT1(np.sqrt(Delta2))#, DN=dp.mff.nucleon.DN_fc)
-        F_our = dp.mff.DT1(np.sqrt(Delta2), DN=dp.mff.nucleon.DN_hz)
+        #F = dp.mff.DT1(np.sqrt(dl2))#, DN=dp.mff.nucleon.DN_fc)
+        F = dp.mff.DT1(np.sqrt(dl2), DN=dp.mff.nucleon.DN_hz)
     elif(name=='DT2'):
-        F_our = dp.mff.DT2(np.sqrt(Delta2))
-    F_wim = wimdf[name]
-    F_hez = hzdf[name]
-    dl2_hz = hzdf['Delta2']
-    namelabel = {
-            'AU'  : r'$A_U(\varDelta^2)$',
-            'AT'  : r'$A_T(\varDelta^2)$',
-            'J'   : r'$J(\varDelta^2)$',
-            'DU'  : r'$D_U(\varDelta^2)$',
-            'DT1' : r'$D_{T1}(\varDelta^2)$',
-            'DT2' : r'$D_{T2}(\varDelta^2)$'
-            }
-    # Make the plot
-    nrows,ncols=1,1
-    fig = py.figure(figsize=(ncols*8,nrows*6), layout='constrained')
-    ax = py.subplot(nrows,ncols,1)
-    if(logscale):
-        F_our = abs(F_our)
-        F_wim = abs(F_wim)
-    ax.plot(Delta2, F_our, '-',  color='xkcd:forest green',  label=r'Ours')
-    ax.plot(Delta2, F_wim, '--', color='xkcd:rich purple',   label=r'Freese and Cosyn')
-    ax.plot(dl2_hz, F_hez, '-.', color='xkcd:midnight blue', label=r'He and Zahed')
+        F = dp.mff.DT2(np.sqrt(dl2))
+    else:
+        F = dl2 * 0
+    return F
+
+namelabel = {
+        'AU'  : r'$A_U(\varDelta^2)$',
+        'AT'  : r'$A_T(\varDelta^2)$',
+        'J'   : r'$J(\varDelta^2)$',
+        'DU'  : r'$D_U(\varDelta^2)$',
+        'DT1' : r'$D_{T1}(\varDelta^2)$',
+        'DT2' : r'$D_{T2}(\varDelta^2)$'
+        }
+
+def plot_one_mff(ax, name):
+    # MFF from other papers
+    df_wc = dp.mff.wim.make_wimffs()
+    fc_hz = dp.mff.hz.make_hzmffs()
+    df_jp = dp.mff.pegg.make_peggmffs()
+    F_wc = df_wc[name]
+    F_hz = fc_hz[name]
+    F_jp = df_jp[name]
+    dl2_wc = df_wc['Delta2']
+    dl2_hz = fc_hz['Delta2']
+    dl2_jp = df_jp['Delta2']
+    # Our MFF
+    dl2 = np.geomspace(1e-6, 1e1, 666)
+    F = select_mff(name, dl2)
+    # Plot
+    ax.plot(dl2,    F,    '-',  linewidth=2, color='xkcd:hunter green', label=r'Ours')
+    ax.plot(dl2_wc, F_wc, '--', linewidth=2, color='xkcd:dark lavender',label=r'Freese and Cosyn')
+    ax.plot(dl2_hz, F_hz, '-.', linewidth=2, color='xkcd:cobalt', label=r'He and Zahed')
+    ax.plot(dl2_jp, F_jp, ':',  linewidth=2, color='xkcd:coral',  label=r'Panteleva \textsl{et al.}')
     ax.set_xlabel(r'$\varDelta^2$ (GeV$^2$)')
     ax.set_ylabel(namelabel[name])
-    _ = ax.legend(prop = { 'size' : 20 })
     ax.set_xscale('log')
-    if(logscale):
-        ax.set_yscale('log')
-    fig.patch.set_alpha(0)
-    fig.savefig('derp.pdf')
-    fig.savefig('{}.pdf'.format(name))
+    if(name=='DT1'):
+        ax.set_ylim((-560,56))
+    ax.set_xlim((1e-6,10))
     return
 
-def plot_AU():
-    wimdf = dp.mff.wim.make_wimffs()
-    Delta2 = wimdf['Delta2']
-    AU = dp.mff.AU(np.sqrt(Delta2))
-    AU_wim = wimdf['AU']
-    # Make the plot
-    nrows,ncols=1,1
+def plot_comparisons():
+    nrows,ncols=2,3
     fig = py.figure(figsize=(ncols*8,nrows*6), layout='constrained')
-    ax = py.subplot(nrows,ncols,1)
-    ax.plot(Delta2, AU,     '-',  color='xkcd:forest green', label=r'Ours')
-    ax.plot(Delta2, AU_wim, '--', color='xkcd:rich purple',  label=r'Freese and Cosyn')
-    ax.set_xlabel(r'$\varDelta^2$ (GeV$^2$)')
-    ax.set_ylabel(r'$A_U(\varDelta^2)$')
-    _ = ax.legend(prop = { 'size' : 20 })
-    ax.set_xscale('log')
+    ax_AU  = py.subplot(nrows,ncols,1)
+    ax_AT  = py.subplot(nrows,ncols,2)
+    ax_J   = py.subplot(nrows,ncols,3)
+    ax_DU  = py.subplot(nrows,ncols,4)
+    ax_DT1 = py.subplot(nrows,ncols,5)
+    ax_DT2 = py.subplot(nrows,ncols,6)
+    plot_one_mff(ax_AU,  'AU')
+    plot_one_mff(ax_AT,  'AT')
+    plot_one_mff(ax_J,   'J')
+    plot_one_mff(ax_DU,  'DU')
+    plot_one_mff(ax_DT1, 'DT1')
+    plot_one_mff(ax_DT2, 'DT2')
+    l = ax_AU.legend(prop = { 'size' : 24 }, loc=3)
     fig.patch.set_alpha(0)
     fig.savefig('derp.pdf')
-    fig.savefig('AU.pdf')
-    return
-
-def plot_AT():
-    wimdf = dp.mff.wim.make_wimffs()
-    Delta2 = wimdf['Delta2']
-    AU = dp.mff.AT(np.sqrt(Delta2))
-    AU_wim = wimdf['AT']
-    # Make the plot
-    nrows,ncols=1,1
-    fig = py.figure(figsize=(ncols*8,nrows*6), layout='constrained')
-    ax = py.subplot(nrows,ncols,1)
-    ax.plot(Delta2, AU,     '-',  color='xkcd:forest green', label=r'Ours')
-    ax.plot(Delta2, AU_wim, '--', color='xkcd:rich purple',  label=r'Freese and Cosyn')
-    ax.set_xlabel(r'$\varDelta^2$ (GeV$^2$)')
-    ax.set_ylabel(r'$A_T(\varDelta^2)$')
-    _ = ax.legend(prop = { 'size' : 20 })
-    ax.set_xscale('log')
-    fig.patch.set_alpha(0)
-    fig.savefig('derp.pdf')
-    fig.savefig('AT.pdf')
+    fig.savefig('deuteron_mff_comparison.pdf')
     return
