546f548eeb569822ac83c82e57a21153ddf3508f
asosa49 <asosa6523@gmail.com>
2025-11-11 20:06:38 -0500

    added point fermion mffs

--- deupack/mff/nucleon/chooser.py
+++ deupack/mff/nucleon/chooser.py
@@ -8,6 +8,7 @@ from . import ba as ba_mod
 from . import hz as hz_mod
 from . import misc as misc_mod
 from . import mit as mit_mod
+from . import point as point_mod
 
 # Add mapping for convenient selection
 NUCLEONFFS = {
@@ -19,6 +20,9 @@ NUCLEONFFS = {
             ),
         'ba' : (
             ba_mod.AN, ba_mod.JN, ba_mod.DN, misc_mod.cN, misc_mod.SN
+            ),
+        'point' : (
+            point_mod.AN, point_mod.JN, point_mod.DN, misc_mod.cN, point_mod.SN
             )
         }
 
---
+++ deupack/mff/nucleon/point.py
@@ -0,0 +1,21 @@
+# point.py
+# Created 2025.11.11 by Alan Sosa
+#
+# This contains point like fermion MFFs
+
+
+def AN(k):
+    ''' AN for point like fermion '''
+    return 1.0
+
+def JN(k):
+    ''' JN for point like fermion '''
+    return 0.5
+
+def SN(k):
+    ''' SN for point like fermion '''
+    return 0.5
+
+def DN(k):
+    ''' DN for point like fermion '''
+    return 0.0
