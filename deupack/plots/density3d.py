9bb7b7bcd804b2c94b4e27da300bf61021727ff4
Adam Freese <adamfreese@proton.me>
2025-12-05 14:18:19 -0500

    Support optinal gridlines

--- deupack/plots/density3d.py
+++ deupack/plots/density3d.py
@@ -16,11 +16,13 @@ import matplotlib as mpl
 # - allows divergent colormaps
 # - allows 2D projections onto planes
 #   - change in input arrays to accommodate this easily
-#   - optional vmax parameter to allow consistent color scales across subplots
+#   - optional vmax2d and vmax3d parameters to allow consistent color scales across subplots
+#   - optional gridlines on walls
 
 def density3d(ax, x, y, z, values,
               decay=2.0, opacity=1.0, cmap=plt.cm.magma,
-              projections=False, divergent=False, vmax=-1, vmax3d=-1,
+              projections=False, divergent=False, grid=True,
+              vmax2d=-1, vmax3d=-1,
               **kwargs):
     ''' Create a density plot for X, Y, Z coordinates and corresponding intensity values.
     Adapted from code by Lorentz Sparrenberg; see:
@@ -49,6 +51,8 @@ def density3d(ax, x, y, z, values,
         Set to True to also plot 2D projections on the panes.
     divergent: bool, optional
         Set to True if the values can go negative and a divergent colormap is used.
+    grid: bool, optional
+        If True, a grid will be drawn on the walls
     vmax: float, optional
         Maximum effective value for scaling the coloarmap in the 2D projections.
         If <0 is passed, then the data will be used to determine the maximum value.
@@ -86,9 +90,11 @@ def density3d(ax, x, y, z, values,
     ax.scatter(x_, y_, z_, c=colors_flattened, zorder=3, **kwargs)
     # Project onto 2D planes if requested
     if(projections):
-        projection2d(ax, x, y, z, values, 'x', cmap=cmap, divergent=divergent, vmax=vmax)
-        projection2d(ax, x, y, z, values, 'y', cmap=cmap, divergent=divergent, vmax=vmax)
-        projection2d(ax, x, y, z, values, 'z', cmap=cmap, divergent=divergent, vmax=vmax)
+        projection2d(ax, x, y, z, values, 'x', cmap=cmap, divergent=divergent, vmax=vmax2d)
+        projection2d(ax, x, y, z, values, 'y', cmap=cmap, divergent=divergent, vmax=vmax2d)
+        projection2d(ax, x, y, z, values, 'z', cmap=cmap, divergent=divergent, vmax=vmax2d)
+        if(grid):
+            add_gridlines(ax)
     return None
 
 # The 2D projection routine called by the 3D density routine ~~~~~~~~~~~~~~~~~~~
@@ -128,7 +134,7 @@ def projection2d(ax, x, y, z, values, axis,
     if(axis=='x'):
         # Get 2D values by integrating out axis
         values2D = np.trapz(values, x=x, axis=0)
-        # Make a meshgrid for non-integreated dimensions
+        # Make a meshgrid for non-integrated dimensions
         y_, z_ = np.meshgrid(y, z, indexing='ij')
         axmin, axmax = ax.get_xlim()
         # Fill an array for the integrated axis at the location its surface should be
@@ -160,17 +166,65 @@ def projection2d(ax, x, y, z, values, axis,
     else:
         colors = cmap(clip(values2D / vmax))
     # Plot surfaces to emulate pcolormesh as best as possible
-    ax.plot_surface(x_, y_, z_, facecolors=colors, rstride=1, cstride=1, shade=False, zorder=2)
+    ax.plot_surface(x_, y_, z_,
+                    facecolors=colors, rstride=1, cstride=1,
+                    shade=False, zorder=1)
+    # Attempt at a grid...
+    ##ax.plot_surface(x_, y_, z_,
+    ##                edgecolor='black',
+    ##                lw=0.5,
+    ##                rstride=8,
+    ##                cstride=8,
+    ##                alpha=0.0)
+    ## ax.grid(True, linestyle='-', color='r', zorder=2)
     # Reset the x, y or z limits in case they were moved
     set_lim((axmin, axmax))
+    return
+
+def add_gridlines(ax):
+    # Get tick locations; they're where we'll make the lines
+    xticks = ax.get_xticks()
+    yticks = ax.get_yticks()
+    zticks = ax.get_zticks()
+    # Get the minimum and maximum x, y and z values for line extents
+    xmin, xmax = ax.get_xlim()
+    ymin, ymax = ax.get_ylim()
+    zmin, zmax = ax.get_zlim()
+    # Gridlines
+    kwargs = {
+            'linewidth' : 1,
+            'zorder'    : 2,
+            'color'     : 'gray',
+            'alpha'     : 0.5
+            }
+    # Along x ticks (bottom & back walls)
+    for x in xticks:
+        if(x > xmin and x < xmax):
+            ax.plot([x,x], [ymin, ymax], [zmin, zmin], '-', **kwargs)
+            ax.plot([x,x], [ymax, ymax], [zmin, zmax], '-', **kwargs)
+    # Along y ticks (bottom & left walls)
+    for y in yticks:
+        if(y > ymin and y < ymax):
+            ax.plot([xmin,xmax], [y, y], [zmin, zmin], '-', **kwargs)
+            ax.plot([xmin,xmin], [y, y], [zmin, zmax], '-', **kwargs)
+    # Along z ticks (back & left walls)
+    for z in zticks:
+        if(z > zmin and z < zmax):
+            ax.plot([xmin,xmax], [ymax, ymax], [z, z], '-', **kwargs)
+            ax.plot([xmin,xmin], [ymin, ymax], [z, z], '-', **kwargs)
+    # Reset axis limits
+    ax.set_xlim((xmin,xmax))
+    ax.set_ylim((ymin,ymax))
+    ax.set_zlim((zmin,zmax))
     return
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # A multi-density plotting routine, tuned to deuteron needs
 
 def multidensity3d(fig, x, y, z, nrows, ncols, *values_list,
-                   labels=None, clabel=None, cmap=plt.cm.magma, divergent=False,
-                   vmax = -1,
+                   labels=None, clabel=None, cmap=plt.cm.magma,
+                   projections=False, divergent=False,
+                   vmax2d = -1, vmax3d=-1,
                    **kwargs):
     ''' Creates multiple 3D density plots in a single figure, along with a
     common colorbar for 2D projections at the bottom.
@@ -202,10 +256,14 @@ def multidensity3d(fig, x, y, z, nrows, ncols, *values_list,
     cmap : mpl.colors.LinearSegmentedColormap, optional
         The colormap used for mapping intensity values to RGB colors.
         Default is plt.cm.magma.
+    projections: bool, optional
+        Set to True to also plot 2D projections on the panes.
     divergent: bool, optional
         Set to True if the values can go negative and a divergent colormap is used.
-    vmax: float, optional
+    vmax2d: float, optional
         Give a positive number to manually set the colorbar scale
+    vmax3d: float, optional
+        Give a positive number to consistently scale 3D density plots
     **kwargs
         Additional keyword arguments to pass to density3d.
         See docstring thereof for options.
@@ -228,6 +286,7 @@ def multidensity3d(fig, x, y, z, nrows, ncols, *values_list,
         ax.set_zlabel('\n'+r'$z$ (fm)')
         ax.grid(False)
         ax.patch.set_alpha(0)
+        ax.computed_zorder = False # TEST
         for axis in [ax.xaxis, ax.yaxis, ax.zaxis]:
             if(divergent):
                 axis.set_pane_color(cmap(0.5))
@@ -237,28 +296,40 @@ def multidensity3d(fig, x, y, z, nrows, ncols, *values_list,
     # Add another axis at the bottom for the colorbar
     axes += [fig.add_subplot(gs[nrows*N,:])]
     # Get the max value of any projections to use for scaling the colormap.
-    # TODO: nicer code for getting vmax
-    if(vmax<0):
+    # TODO: nicer code for getting vmaxes
+    if(vmax2d<0):
         maxes = [
                 [ abs(np.trapz(p, x=[x,y,z][axis], axis=axis)).max() for p in values_list ]
                 for axis in [0,1,2]
                 ]
-        vmax = np.max(maxes)
+        vmax2d = np.max(maxes)
+    if(vmax3d<0):
+        maxes = [
+                abs(p).max() for p in values_list
+                ]
+        vmax3d = np.max(maxes)
     # Add each of the 3D densities
     for n in range(len(values_list)):
         density3d(axes[n], x, y, z, values_list[n],
-                  vmax=vmax, cmap=cmap, divergent=divergent, **kwargs)
+                  vmax2d=vmax2d, vmax3d=vmax3d, cmap=cmap,
+                  projections=projections, divergent=divergent,
+                  **kwargs)
     # Some labels
     if(labels is not None):
         bbox = dict(facecolor='white', alpha=0.76, edgecolor='black', boxstyle='round,pad=0.5')
         for n in range(len(values_list)):
             axes[n].text2D(0.05,0.95, labels[n], fontsize=36, transform=axes[n].transAxes, bbox=bbox)
     # Colorbar!
-    if(divergent):
-        vmin = -vmax
+    # Values based on 2D projections if they're present; 3D otherwise
+    if(projections):
+        vmax_c = vmax2d
     else:
-        vmin = 0
-    norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
+        vmax_c = vmax3d
+    if(divergent):
+        vmin_c = -vmax_c
+    else:
+        vmin_c = 0
+    norm = mpl.colors.Normalize(vmin=vmin_c, vmax=vmax_c)
     cbar = fig.colorbar(
             mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
             cax=axes[-1],
