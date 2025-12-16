b11445118958c3f5df42c25a5579fab178f52647
Adam Freese <adamfreese@proton.me>
2025-12-15 21:13:32 -0500

    Use bool flags to control LO/IA in others EMTFFs

--- deupack/emtff/hz.py
+++ deupack/emtff/hz.py
@@ -2,7 +2,7 @@
 # Created 2025.10.10 by Adam Freese
 #
 # This module reads in Fangcheng's data file for his GFF calculations,
-# and converts them to the MFFs in Cosyn/Freese/Sosa.
+# and converts them to the EMTFFs in Cosyn/Freese/Sosa.
 #
 # The form factors this module looks at are from:
 #   Fancheng He and Ismail Zahed
@@ -17,9 +17,9 @@ from ..constants import Md
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-def make_hzffs():
+def make_hzffs(impulse_approximation=False):
     ''' Convert He and Zahed's GFFs into those of Cosyn, Freese and Sosa. '''
-    df = read_emtffs()
+    df = read_emtffs(impulse_approximation=impulse_approximation)
     t = df['t']
     A = df['A']
     Q = df['Q']
@@ -43,69 +43,27 @@ def make_hzffs():
         })
     return new_df
 
-
-def make_hzffsIA():
-    ''' Convert He and Zahed's GFFs into those of Cosyn, Freese and Sosa. '''
-    df = read_emtffsIA()
-    t = df['t']
-    A = df['A']
-    Q = df['Q']
-    J = df['J']
-    D0 = df['D0']
-    D2 = df['D2']
-    D3 = df['D3']
-    AU = A
-    AT = -Q
-    DU = D0
-    DT2 = -D2
-    DT1 = 2*Md**2/t * D3
-    new_df = pd.DataFrame({
-        'Delta2' : -t,
-        'AU'     : AU,
-        'AT'     : AT,
-        'J'      : J,
-        'DU'     : DU,
-        'DT1'    : DT1,
-        'DT2'    : DT2
-        })
-    return new_df
-
-def read_emtffs():
-    ''' Read EMT data from the tables Fangcheng provided. '''
+def read_emtffs(impulse_approximation=False):
+    ''' Read EMT data from the tables Fangcheng provided.
+    ----------
+    Input:
+        impulse_approximation : bool, optional
+            if True, use the impulse approximation (IA) tables
+    Output:
+        pandas.DataFrame
+    '''
     path = Path(__file__).parent.parent / 'data/hz'
-    df_A =  pd.read_csv(path / "data_A.txt",  header=None, sep='\s+')
-    df_J =  pd.read_csv(path / "data_J.txt",  header=None, sep='\s+')
-    df_Q =  pd.read_csv(path / "data_Q.txt",  header=None, sep='\s+')
-    df_D0 = pd.read_csv(path / "data_D0.txt", header=None, sep='\s+')
-    df_D2 = pd.read_csv(path / "data_D2.txt", header=None, sep='\s+')
-    df_D3 = pd.read_csv(path / "data_D3.txt", header=None, sep='\s+')
-    t = -df_A[0]
-    A = df_A[1]
-    J = df_J[1]
-    Q = df_Q[1]
-    D0 = df_D0[1]
-    D2 = df_D2[1]
-    D3 = df_D3[1]
-    df = pd.DataFrame({
-        't' : t,
-        'A' : A,
-        'Q' : Q,
-        'J' : J,
-        'D0' : D0,
-        'D2' : D2,
-        'D3' : D3
-        })
-    return df
-
-def read_emtffsIA():
-    ''' Read EMT data from the tables Fangcheng provided. '''
-    path = Path(__file__).parent.parent / 'data/hz'
-    df_A =  pd.read_csv(path / "data_A_IA.txt",  header=None, sep='\s+')
-    df_J =  pd.read_csv(path / "data_J_IA.txt",  header=None, sep='\s+')
-    df_Q =  pd.read_csv(path / "data_Q_IA.txt",  header=None, sep='\s+')
-    df_D0 = pd.read_csv(path / "data_D0_IA.txt", header=None, sep='\s+')
-    df_D2 = pd.read_csv(path / "data_D2_IA.txt", header=None, sep='\s+')
-    df_D3 = pd.read_csv(path / "data_D3_IA.txt", header=None, sep='\s+')
+    def gff_path(name):
+        if(impulse_approximation):
+            return path / "data_{}_IA.txt".format(name)
+        else:
+            return path / "data_{}.txt".format(name)
+    df_A =  pd.read_csv(gff_path('A'),  header=None, sep='\s+')
+    df_J =  pd.read_csv(gff_path('J'),  header=None, sep='\s+')
+    df_Q =  pd.read_csv(gff_path('Q'),  header=None, sep='\s+')
+    df_D0 = pd.read_csv(gff_path('D0'), header=None, sep='\s+')
+    df_D2 = pd.read_csv(gff_path('D2'), header=None, sep='\s+')
+    df_D3 = pd.read_csv(gff_path('D3'), header=None, sep='\s+')
     t = -df_A[0]
     A = df_A[1]
     J = df_J[1]
--- deupack/emtff/pegg.py
+++ deupack/emtff/pegg.py
@@ -2,7 +2,7 @@
 # Created 2025.10.23 by Adam Freese
 #
 # This module reads in Julia's data files for her GFF calculations,
-# and converts them to the MFFs in Cosyn/Freese/Sosa.
+# and converts them to the EMTFFs in Cosyn/Freese/Sosa.
 #
 # The form factors this module looks at are from:
 #   J.Yu. Panteleeva, E. Epelbaum, A.M. Gasparyan, J. Gegelia
@@ -17,9 +17,9 @@ from ..constants import Md
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-def make_peggffs():
-    ''' Convert Panteleeva et al.'s MFFs into those of Cosyn, Freese and Sosa. '''
-    df = read_emtffs()
+def make_peggffs(leading_order=False):
+    ''' Convert Panteleeva et al.'s GFFs into those of Cosyn, Freese and Sosa. '''
+    df = read_emtffs(leading_order=leading_order)
     t = df['t']
     E0 = df['E0']
     E2 = df['E2']
@@ -43,84 +43,43 @@ def make_peggffs():
         })
     return new_df
 
-
-
-def make_peggffsLO():
-    ''' Convert Panteleeva et al.'s MFFs into those of Cosyn, Freese and Sosa. '''
-    df = read_emtffsLO()
-    t = df['t']
-    E0 = df['E0']
-    E2 = df['E2']
-    D0 = df['D0']
-    D2 = df['D2']
-    D3 = df['D3']
-    J = df['J']
-    AU = E0
-    AT = -2*E2
-    DU = -4*D0
-    DT1 = 8*D3
-    DT2 = -2*D2
-    new_df = pd.DataFrame({
-        'Delta2' : -t,
-        'AU'     : AU,
-        'AT'     : AT,
-        'J'      : J,
-        'DU'     : DU,
-        'DT1'    : DT1,
-        'DT2'    : DT2
-        })
-    return new_df
-
-
-def read_emtffs():
-    ''' Read EMT data from the tables Fangcheng provided. '''
+def read_emtffs(leading_order=False):
+    ''' Read EMT data from the tables Julia provided.
+    ----------
+    Input:
+        - leading_order : bool, optional
+            if True, use leading order calculation;
+            otherwise, highest order available is used
+    Output:
+        pandas.DataFrame
+    '''
     path = Path(__file__).parent.parent / 'data/pegg'
     # c8 and c9 provided by Julia Panteleeva (private communication)
     c8 = -2.77/1000 # MeV**-1
     c9 = 0
     # Use the highest-order data available for each MFF
-    df_E0 = pd.read_csv(path / "E0_nnlo.txt", comment='#')
-    df_E2 = pd.read_csv(path / "E2_nnlo.txt", comment='#')
-    df_D0 = pd.read_csv(path / "D0_nlo.txt",  comment='#')
-    df_D2 = pd.read_csv(path / "D2_nlo.txt",  comment='#')
-    df_D3 = pd.read_csv(path / "D3_nlo.txt",  comment='#')
-    df_J  = pd.read_csv(path / "J_lo.txt",    comment='#')
+    max_order = {
+            'E0' : 'nnlo',
+            'E2' : 'nnlo',
+            'D0' : 'nlo',
+            'D2' : 'nlo',
+            'D3' : 'nlo',
+            'J'  : 'lo'
+            }
+    def gff_path(name):
+        if(leading_order):
+            return path / "{}_lo.txt".format(name)
+        else:
+            return path / "{}_{}.txt".format(name, max_order[name])
+    df_E0 = pd.read_csv(gff_path('E0'), comment='#')
+    df_E2 = pd.read_csv(gff_path('E2'), comment='#')
+    df_D0 = pd.read_csv(gff_path('D0'), comment='#')
+    df_D2 = pd.read_csv(gff_path('D2'), comment='#')
+    df_D3 = pd.read_csv(gff_path('D3'), comment='#')
+    df_J  = pd.read_csv(gff_path('J'),  comment='#')
     t  = -(df_E0['q (MeV)']/1000)**2
     E0 = df_E0['E0'] + df_E0['E0c8']*c8 + df_E0['E0c9']*c9
     E2 = df_E2['E2'] + df_E2['E2c8']*c8 + df_E2['E2c9']*c9
-    D0 = df_D0['D0'] + df_D0['D0c8']*c8
-    D2 = df_D2['D2'] + df_D2['D2c8']*c8
-    D3 = df_D3['D3'] + df_D3['D3c8']*c8
-    J  =  df_J['J']  +  df_J['Jc9'] *c9
-    df = pd.DataFrame({
-        't' : t,
-        'E0' : E0,
-        'E2' : E2,
-        'D0' : D0,
-        'D2' : D2,
-        'D3' : D3,
-        'J' : J
-        })
-    return df
-
-
-
-def read_emtffsLO():
-    ''' Read EMT data from the tables Fangcheng provided. '''
-    path = Path(__file__).parent.parent / 'data/pegg'
-    # c8 and c9 provided by Julia Panteleeva (private communication)
-    c8 = -2.77/1000 # MeV**-1
-    c9 = 0
-    # Use the highest-order data available for each MFF
-    df_E0 = pd.read_csv(path / "E0_lo.txt", comment='#')
-    df_E2 = pd.read_csv(path / "E2_lo.txt", comment='#')
-    df_D0 = pd.read_csv(path / "D0_lo.txt",  comment='#')
-    df_D2 = pd.read_csv(path / "D2_lo.txt",  comment='#')
-    df_D3 = pd.read_csv(path / "D3_lo.txt",  comment='#')
-    df_J  = pd.read_csv(path / "J_lo.txt",    comment='#')
-    t  = -(df_E0['q (MeV)']/1000)**2
-    E0 = df_E0['E0'] 
-    E2 = df_E2['E2'] 
     D0 = df_D0['D0'] + df_D0['D0c8']*c8
     D2 = df_D2['D2'] + df_D2['D2c8']*c8
     D3 = df_D3['D3'] + df_D3['D3c8']*c8
