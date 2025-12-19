e77f61e3a7c00de67ab47815e471dd00a71c5d0f
Adam Freese <adamfreese@proton.me>
2025-12-19 17:24:28 -0500

    Change view window for DT2

--- deupack/plots/paperplots.py
+++ deupack/plots/paperplots.py
@@ -523,11 +523,10 @@ def _group_comparison_panel(ax, name):
             bbox=bbox
             )
     ax.set_xscale('log')
-    # Limit the window for DT1 and DT2, in light of divergences in some cases
+    # Limit the window for DT1 and DT2 to improve visibility
     if(name=='DT1'):
         ax.set_ylim((-560,560))
     if(name=='DT2'):
-        #ax.set_ylim((-1.37,0.69))
         ax.set_ylim((-0.17,0.59))
     ax.set_xlim((1e-6,10))
     return
