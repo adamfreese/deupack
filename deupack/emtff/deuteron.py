00020733d09022a45d11ad49974375291bf905bd
Adam Freese <adamfreese@proton.me>
2026-02-03 17:06:10 -0500

    Further refactor so NFF objects are passed around
    Instead of passing nucleon form factors as a list, NFF objects which
    contain the form factors as methods are passed around. This will allow
    future changes to be made more easily, especially if additional
    properties of a nucleon are called for.

--- deupack/emtff/deuteron.py
+++ deupack/emtff/deuteron.py
@@ -55,61 +55,61 @@ def AU(k, wf=wf_default, nff='ba'):
         but the 'paper' formula is significantly slower.
     '''
     dwf = choose_wf(wf)
-    AN, *_ = choose_nff(nff)
-    return _AU(k, dwf=dwf, AN=AN)
+    _nff = choose_nff(nff)
+    return _AU(k, dwf=dwf, nff=_nff)
 
 def AT(k, wf=wf_default, nff='ba'):
     ''' The EMT form factor AT.
     See docstring of AU for more info.
     '''
     dwf = choose_wf(wf)
-    AN, *_ = choose_nff(nff)
-    return _AT(k, dwf=dwf, AN=AN)
+    _nff = choose_nff(nff)
+    return _AT(k, dwf=dwf, nff=_nff)
 
 def DU(k, wf=wf_default, nff='ba'):
     ''' The EMT form factor DU.
     See docstring of AU for more info.
     '''
     dwf = choose_wf(wf)
-    AN, JN, DN, *_ = choose_nff(nff)
-    return _DU(k, dwf=dwf, AN=AN, JN=JN, DN=DN)
+    _nff = choose_nff(nff)
+    return _DU(k, dwf=dwf, nff=_nff)
 
 def DT1(k, wf=wf_default, nff='ba'):
     ''' The EMT form factor DT1.
     See docstring of AU for more info.
     '''
     dwf = choose_wf(wf)
-    AN, JN, DN, *_ = choose_nff(nff)
-    return _DT1(k, dwf=dwf, AN=AN, JN=JN, DN=DN)
+    _nff = choose_nff(nff)
+    return _DT1(k, dwf=dwf, nff=_nff)
 
 def DT2(k, wf=wf_default, nff='ba'):
     ''' The EMT form factor DT2.
     See docstring of AU for more info.
     '''
     dwf = choose_wf(wf)
-    AN, JN, *_ = choose_nff(nff)
-    return _DT2(k, dwf=dwf, AN=AN, JN=JN)
+    _nff = choose_nff(nff)
+    return _DT2(k, dwf=dwf, nff=_nff)
 
 def cU(k, wf=wf_default, nff='ba', formula='fast'):
     ''' The EMT form factor cU.
     See docstring of AU for more info.
     '''
     dwf = choose_wf(wf)
-    AN, _, _, cN, _ = choose_nff(nff)
+    _nff = choose_nff(nff)
     # Need to change rmin from 0 to 1e-2 for Yukawa parametrizations,
     # because of an instability at small r
     rmin = 0
     if(wf=='paris' or wf=='cdbonn'):
         rmin = 1e-2
-    return _cU(k, dwf=dwf, AN=AN, cN=cN, rmin=rmin, formula=formula)
+    return _cU(k, dwf=dwf, nff=_nff, rmin=rmin, formula=formula)
 
 def cT1(k, wf=wf_default, nff='ba', formula='fast'):
     ''' The EMT form factor cT1.
     See docstring of AU for more info.
     '''
     dwf = choose_wf(wf)
-    AN, JN, _, cN, _ = choose_nff(nff)
-    return _cT1(k, dwf=dwf, AN=AN, JN=JN, cN=cN, formula=formula)
+    _nff = choose_nff(nff)
+    return _cT1(k, dwf=dwf, nff=_nff, formula=formula)
 
 def cT2(k, wf=wf_default, nff='ba', formula='fast'):
     ''' The EMT form factor cT2.
@@ -118,35 +118,35 @@ def cT2(k, wf=wf_default, nff='ba', formula='fast'):
     dwf = choose_wf(wf)
     # Need to change rmin from 0 to 1e-2 for Yukawa parametrizations,
     # because of an instability at small r
-    AN, JN, *_ = choose_nff(nff)
+    _nff = choose_nff(nff)
     rmin = 0
     if(wf=='paris' or wf=='cdbonn'):
         rmin =  1e-2
-    return _cT2(k, dwf=dwf, AN=AN, JN=JN, rmin=rmin, formula=formula)
+    return _cT2(k, dwf=dwf, nff=_nff, rmin=rmin, formula=formula)
 
 def J(k, wf=wf_default, nff='ba'):
     ''' The EMT form factor J.
     See docstring of AU for more info.
     '''
     dwf = choose_wf(wf)
-    AN, JN, *_ = choose_nff(nff)
-    return _J(k, dwf=dwf, AN=AN, JN=JN)
+    _nff = choose_nff(nff)
+    return _J(k, dwf=dwf, nff=_nff)
 
 def S(k, wf=wf_default, nff='ba'):
     ''' The EMT form factor S.
     See docstring of AU for more info.
     '''
     dwf = choose_wf(wf)
-    _, _, _, _, SN = choose_nff(nff)
-    return _S(k, dwf=dwf, SN=SN)
+    _nff = choose_nff(nff)
+    return _S(k, dwf=dwf, nff=_nff)
 
 def sbar(k, wf=wf_default, nff='ba'):
     ''' The EMT form factor S.
     See docstring of AU for more info.
     '''
     dwf = choose_wf(wf)
-    _, _, _, _, SN = choose_nff(nff)
-    return _sbar(k, dwf=dwf, SN=SN)
+    _nff = choose_nff(nff)
+    return _sbar(k, dwf=dwf, nff=_nff)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Under-the-hood implementation details for the EMTFFs:
@@ -154,34 +154,34 @@ def sbar(k, wf=wf_default, nff='ba'):
 #    Parallelization of the integration requires the integrands to be defined
 #    as top-level (rather than nested) functions.
 
-def _AU_integrand(r, k, dwf, AN):
+def _AU_integrand(r, k, dwf, nff):
     kfm = k/hbar
-    intd = AN(k) * (dwf.u(r)**2 + dwf.w(r)**2)/2 * jn(0, kfm*r/2)
+    intd = nff.AN(k) * (dwf.u(r)**2 + dwf.w(r)**2)/2 * jn(0, kfm*r/2)
     return intd
 
-def _AT_integrand(r, k, dwf, AN):
+def _AT_integrand(r, k, dwf, nff):
     kfm = k/hbar
-    intd = AN(k) * jn(2, kfm*r/2)*(
+    intd = nff.AN(k) * jn(2, kfm*r/2)*(
             2*sqrt(2)*dwf.u(r)*dwf.w(r) - dwf.w(r)**2
             ) * 6*mN**2/k**2
     return intd
 
-def _DU_integrand(r, k, dwf, AN, JN, DN):
+def _DU_integrand(r, k, dwf, nff):
     kfm = k/hbar
-    A_piece = 4*AN(k)/kfm**2*jn(2,kfm*r/2)*(
+    A_piece = 4*nff.AN(k)/kfm**2*jn(2,kfm*r/2)*(
             (2*dwf.u(r)**2+8*dwf.w(r)**2)/r**2
             - (dwf.u(r)*dwf.u1(r)+dwf.w(r)*dwf.w1(r))/r
             + dwf.u(r)*dwf.u2(r) + dwf.w(r)*dwf.w2(r)
             - dwf.u1(r)**2 - dwf.w1(r)**2
             )
-    J_piece = -12*JN(k)/kfm*jn(1,kfm*r/2)*dwf.w(r)**2/r
-    D_piece = 2*DN(k)*jn(0,kfm*r/2)*(dwf.u(r)**2 + dwf.w(r)**2)
+    J_piece = -12*nff.JN(k)/kfm*jn(1,kfm*r/2)*dwf.w(r)**2/r
+    D_piece = 2*nff.DN(k)*jn(0,kfm*r/2)*(dwf.u(r)**2 + dwf.w(r)**2)
     intd = A_piece + J_piece + D_piece
     return intd
 
-def _DT1_integrand(r, k, dwf, AN, JN, DN):
+def _DT1_integrand(r, k, dwf, nff):
     kfm = k/hbar
-    A_piece = 48*mNfm**2*AN(k)/kfm**4*jn(4,kfm*r/2)*(
+    A_piece = 48*mNfm**2*nff.AN(k)/kfm**4*jn(4,kfm*r/2)*(
             sqrt(2)*(dwf.u(r)*dwf.w2(r) + dwf.w(r)*dwf.u2(r)
                      - 2*dwf.u1(r)*dwf.w1(r))
             - dwf.w(r)*dwf.w2(r) + dwf.w1(r)**2
@@ -189,25 +189,25 @@ def _DT1_integrand(r, k, dwf, AN, JN, DN):
                + dwf.w(r)*dwf.w1(r))/r
             + 6*(2*sqrt(2)*dwf.u(r) - dwf.w(r))*dwf.w(r)/r**2
             )
-    J_piece = 96*mNfm**2*JN(k)/kfm**3*jn(3,kfm*r/2)*(
+    J_piece = 96*mNfm**2*nff.JN(k)/kfm**3*jn(3,kfm*r/2)*(
             sqrt(2)*(dwf.u(r)*dwf.w1(r) - dwf.w(r)*dwf.u1(r))
             - (2*sqrt(2)*dwf.u(r) - dwf.w(r))*dwf.w(r)/r
             )
-    D_piece = 24*mNfm**2*DN(k)/kfm**2*jn(2,kfm*r/2)*(
+    D_piece = 24*mNfm**2*nff.DN(k)/kfm**2*jn(2,kfm*r/2)*(
             2*sqrt(2)*dwf.u(r)*dwf.w(r) - dwf.w(r)**2
             )
     intd = A_piece + J_piece + D_piece
     return intd
 
-def _DT2_integrand(r, k, dwf, AN, JN):
+def _DT2_integrand(r, k, dwf, nff):
     kfm = k/hbar
-    A_piece = 12*AN(k)/kfm**3*jn(3,kfm*r/2)*(
+    A_piece = 12*nff.AN(k)/kfm**3*jn(3,kfm*r/2)*(
             ( (2*sqrt(2)*dwf.u2(r) - dwf.w2(r))*dwf.w(r)
                 - (2*sqrt(2)*dwf.u1(r) - dwf.w1(r))*dwf.w1(r) )/r
             + (2*sqrt(2)*dwf.u(r) + 5*dwf.w(r))*dwf.w1(r)/r**2
             - 18*dwf.w(r)**2/r**3
             )
-    J_piece = 6*JN(k)/kfm**2*jn(2,kfm*r/2)*(
+    J_piece = 6*nff.JN(k)/kfm**2*jn(2,kfm*r/2)*(
             sqrt(2)*(dwf.u(r)*dwf.w2(r) - dwf.w(r)*dwf.u2(r))
             - 4*(sqrt(2)*dwf.u1(r) + dwf.w1(r))*dwf.w(r)/r
             - (2*sqrt(2)*dwf.u(r) - dwf.w(r))*dwf.w(r)/r**2
@@ -216,27 +216,27 @@ def _DT2_integrand(r, k, dwf, AN, JN):
     intd = A_piece + J_piece
     return intd
 
-def _cU_integrand(r, k, dwf, AN, cN):
+def _cU_integrand(r, k, dwf, nff):
     # Uses IBP to get rid of third derivatives of u(r) and w(r)
     kfm = k/hbar
-    A_piece_1 = AN(k)/(mNfm**2*kfm)*jn(1,kfm*r/2)*(
+    A_piece_1 = nff.AN(k)/(mNfm**2*kfm)*jn(1,kfm*r/2)*(
             dwf.u1(r)*dwf.u2(r) - dwf.u(r)*dwf.u2(r)/r
             + dwf.w1(r)*dwf.w2(r) - dwf.w(r)*dwf.w2(r)/r
             - 6*dwf.w(r)**2/r**3
             )
-    A_piece_0 = AN(k)/(4*mNfm**2)*jn(0,kfm*r/2)*(
+    A_piece_0 = nff.AN(k)/(4*mNfm**2)*jn(0,kfm*r/2)*(
             dwf.u(r)*dwf.u2(r) + dwf.w(r)*dwf.w2(r)
             )
-    c_piece = cN(k)/2*jn(0,kfm*r/2)*(
+    c_piece = nff.cN(k)/2*jn(0,kfm*r/2)*(
             dwf.u(r)**2 + dwf.w(r)**2
             )
     intd = A_piece_1 + A_piece_0 + c_piece
     return intd
 
-def _cT1_integrand(r, k, dwf, AN, JN, cN):
+def _cT1_integrand(r, k, dwf, nff):
     # Uses IBP to get rid of third derivatives of u(r) and w(r)
     kfm = k/hbar
-    A_piece_3 = 12*AN(k)/kfm**3*jn(3,kfm*r/2)*(
+    A_piece_3 = 12*nff.AN(k)/kfm**3*jn(3,kfm*r/2)*(
             sqrt(2)*(dwf.u1(r)*dwf.w2(r) + dwf.w1(r)*dwf.u2(r))
             - dwf.w1(r)*dwf.w2(r)
             - sqrt(2)*(dwf.u(r)*dwf.w2(r) + 3*dwf.w(r)*dwf.u2(r))/r
@@ -244,22 +244,22 @@ def _cT1_integrand(r, k, dwf, AN, JN, cN):
             + 3*sqrt(2)*(dwf.u(r)*dwf.w1(r)-dwf.w(r)*dwf.u1(r))/r**2
             - 6*(2*sqrt(2)*dwf.u(r)*dwf.w(r)-dwf.w(r)**2)/r**3
             )
-    A_piece_2 = 3*AN(k)/kfm**2*jn(2,kfm*r/2)*(
+    A_piece_2 = 3*nff.AN(k)/kfm**2*jn(2,kfm*r/2)*(
             + sqrt(2)*(dwf.u(r)*dwf.w2(r) + dwf.w(r)*dwf.u2(r))
             - dwf.w(r)*dwf.w2(r)
             )
-    J_piece = 6*sqrt(2)*JN(k)*jn(2,kfm*r/2)/kfm**2*(
+    J_piece = 6*sqrt(2)*nff.JN(k)*jn(2,kfm*r/2)/kfm**2*(
             dwf.w(r)*dwf.u2(r) - dwf.u(r)*dwf.w2(r) + 6*dwf.u(r)*dwf.w(r)/r**2
             )
-    c_piece = 6*mN**2/k**2 * cN(k) * jn(2,kfm*r/2)*(
+    c_piece = 6*mN**2/k**2 * nff.cN(k) * jn(2,kfm*r/2)*(
             2*sqrt(2)*dwf.u(r)*dwf.w(r) - dwf.w(r)**2
             )
     return A_piece_3 + A_piece_2 + J_piece + c_piece
 
-def _cT2_integrand(r, k, dwf, AN, JN):
+def _cT2_integrand(r, k, dwf, nff):
     # Uses IBP to get rid of third derivatives of u(r) and w(r)
     kfm = k/hbar
-    A_piece_2 = 6*AN(k)/(mNfm**2*kfm**2)*jn(2,kfm*r/2)*(
+    A_piece_2 = 6*nff.AN(k)/(mNfm**2*kfm**2)*jn(2,kfm*r/2)*(
             (sqrt(2)*(dwf.u1(r)*dwf.w2(r) + dwf.w1(r)*dwf.u2(r))
              - dwf.w1(r)*dwf.w2(r))/r
             - sqrt(2)*(dwf.u(r)*dwf.w2(r) + 3*dwf.w(r)*dwf.u2(r))/r**2
@@ -267,33 +267,33 @@ def _cT2_integrand(r, k, dwf, AN, JN):
             - 6*sqrt(2)*dwf.w(r)*dwf.u1(r)/r**3
             + 6*(sqrt(2)*dwf.u(r)*dwf.w(r) + dwf.w(r)**2)/r**4
             )
-    A_piece_1 = 3*AN(k)/(2*mNfm**2*kfm)*jn(1,kfm*r/2)*(
+    A_piece_1 = 3*nff.AN(k)/(2*mNfm**2*kfm)*jn(1,kfm*r/2)*(
             2*sqrt(2)*dwf.w(r)*dwf.u2(r)
             - dwf.w(r)*dwf.w2(r)
             )/r
-    J_piece = -3*sqrt(2)/(4*mNfm**2)*jn(2,kfm*r/2)*JN(k)*(
+    J_piece = -3*sqrt(2)/(4*mNfm**2)*jn(2,kfm*r/2)*nff.JN(k)*(
             dwf.u(r)*dwf.w2(r) - dwf.w(r)*dwf.u2(r) - 6*dwf.u(r)*dwf.w(r)/r**2
             )
     return A_piece_2 + A_piece_1 + J_piece
 
-def _J_integrand(r, k, dwf, AN, JN):
+def _J_integrand(r, k, dwf, nff):
     kfm = k/hbar
-    A_piece = 9/2*AN(k)/kfm * jn(1,kfm*r/2) * dwf.w(r)**2/r
-    J0_piece = JN(k)*jn(0,kfm*r/2)*(dwf.u(r)**2 - dwf.w(r)**2/2)
-    J2_piece = JN(k)*jn(2,kfm*r/2)*(dwf.w(r)**2 + sqrt(2)*dwf.u(r)*dwf.w(r))/2
+    A_piece = 9/2*nff.AN(k)/kfm * jn(1,kfm*r/2) * dwf.w(r)**2/r
+    J0_piece = nff.JN(k)*jn(0,kfm*r/2)*(dwf.u(r)**2 - dwf.w(r)**2/2)
+    J2_piece = nff.JN(k)*jn(2,kfm*r/2)*(dwf.w(r)**2 + sqrt(2)*dwf.u(r)*dwf.w(r))/2
     intd = A_piece + J0_piece + J2_piece
     return intd
 
-def _S_integrand(r, k, dwf, SN):
+def _S_integrand(r, k, dwf, nff):
     kfm = k/hbar
-    S0_piece = SN(k)*jn(0,kfm*r/2)*(dwf.u(r)**2 - dwf.w(r)**2/2)
-    S2_piece = SN(k)*jn(2,kfm*r/2)*(dwf.w(r)**2 + sqrt(2)*dwf.u(r)*dwf.w(r))/2
+    S0_piece = nff.SN(k)*jn(0,kfm*r/2)*(dwf.u(r)**2 - dwf.w(r)**2/2)
+    S2_piece = nff.SN(k)*jn(2,kfm*r/2)*(dwf.w(r)**2 + sqrt(2)*dwf.u(r)*dwf.w(r))/2
     intd = S0_piece + S2_piece
     return intd
 
-def _sbar_integrand(r, k, dwf, SN):
+def _sbar_integrand(r, k, dwf, nff):
     kfm = k/hbar
-    intd = -6*sqrt(2)*SN(k)/kfm**2*jn(2,kfm*r/2)*(
+    intd = -6*sqrt(2)*nff.SN(k)/kfm**2*jn(2,kfm*r/2)*(
             dwf.u(r)*dwf.w2(r) - dwf.u2(r)*dwf.w(r) - 6*dwf.u(r)*dwf.w(r)/r**2
             )
     return intd
@@ -301,18 +301,18 @@ def _sbar_integrand(r, k, dwf, SN):
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # The formulas for cbarU, cbarT1 and cbarT2 given in the paper
 
-def _cU_integrand_paper(r, k, dwf, AN, cN):
+def _cU_integrand_paper(r, k, dwf, nff):
     kfm = k/hbar
-    A_piece = AN(k)*jn(1, kfm*r/2)/(2*mNfm**2*kfm) * (
+    A_piece = nff.AN(k)*jn(1, kfm*r/2)/(2*mNfm**2*kfm) * (
             dwf.u1(r)*dwf.u2(r) + dwf.w1(r)*dwf.w2(r)
             - dwf.u(r)*dwf.u3(r) - dwf.w(r)*dwf.w3(r) - 12*dwf.w(r)**2/r**3
             )
-    c_piece = cN(k)*jn(0, kfm*r/2) * (dwf.u(r)**2 + dwf.w(r)**2)/2
+    c_piece = nff.cN(k)*jn(0, kfm*r/2) * (dwf.u(r)**2 + dwf.w(r)**2)/2
     return A_piece + c_piece
 
-def _cT1_integrand_paper(r, k, dwf, AN, JN, cN):
+def _cT1_integrand_paper(r, k, dwf, nff):
     kfm = k/hbar
-    A_piece = 6*AN(k)/kfm**3*jn(3,kfm*r/2)*(
+    A_piece = 6*nff.AN(k)/kfm**3*jn(3,kfm*r/2)*(
             sqrt(2)*(dwf.u1(r)*dwf.w2(r) + dwf.w1(r)*dwf.u2(r)
                         - dwf.u(r)*dwf.w3(r) - dwf.w(r)*dwf.u3(r))
             + dwf.w(r)*dwf.w3(r) - dwf.w1(r)*dwf.w2(r)
@@ -320,24 +320,24 @@ def _cT1_integrand_paper(r, k, dwf, AN, JN, cN):
             + 6*sqrt(2)*(dwf.u(r)*dwf.w1(r)-dwf.w(r)*dwf.u1(r))/r**2
             -12*(2*sqrt(2)*dwf.u(r)*dwf.w(r)-dwf.w(r)**2)/r**3
             )
-    J_piece = 6*sqrt(2)*JN(k)*jn(2,kfm*r/2)/kfm**2*(
+    J_piece = 6*sqrt(2)*nff.JN(k)*jn(2,kfm*r/2)/kfm**2*(
             dwf.w(r)*dwf.u2(r) - dwf.u(r)*dwf.w2(r) + 6*dwf.u(r)*dwf.w(r)/r**2
             )
-    c_piece = 6*mN**2/k**2 * cN(k) * jn(2,kfm*r/2)*(
+    c_piece = 6*mN**2/k**2 * nff.cN(k) * jn(2,kfm*r/2)*(
             2*sqrt(2)*dwf.u(r)*dwf.w(r) - dwf.w(r)**2
             )
     return A_piece + J_piece + c_piece
 
-def _cT2_integrand_paper(r, k, dwf, AN, JN):
+def _cT2_integrand_paper(r, k, dwf, nff):
     kfm = k/hbar
-    A_piece = -3*AN(k)/(mNfm**2*kfm**2)*jn(2,kfm*r/2)*(
+    A_piece = -3*nff.AN(k)/(mNfm**2*kfm**2)*jn(2,kfm*r/2)*(
             (2*sqrt(2)*(dwf.w(r)*dwf.u3(r)-dwf.u1(r)*dwf.w2(r))
              -dwf.w(r)*dwf.w3(r)+dwf.w1(r)*dwf.w2(r))/r
             +(2*sqrt(2)*(dwf.u(r)*dwf.w2(r)-dwf.w(r)*dwf.u2(r)))/r**2
             +12*sqrt(2)*dwf.w(r)*dwf.u1(r)/r**3
             -12*(sqrt(2)*dwf.u(r)*dwf.w(r)+dwf.w(r)**2)/r**4
             )
-    J_piece = -3*sqrt(2)/(4*mNfm**2)*jn(2,kfm*r/2)*JN(k)*(
+    J_piece = -3*sqrt(2)/(4*mNfm**2)*jn(2,kfm*r/2)*nff.JN(k)*(
             dwf.u(r)*dwf.w2(r) - dwf.w(r)*dwf.u2(r) - 6*dwf.u(r)*dwf.w(r)/r**2
             )
     return A_piece + J_piece
@@ -348,46 +348,46 @@ def _cT2_integrand_paper(r, k, dwf, AN, JN):
 #    quad_vec achieves good speed for parallel calculation of form factors
 #    at multiple k values. It's also parallelizable.
 
-def _AU(k, dwf, AN, rmin=0, rmax=np.inf):
+def _AU(k, dwf, nff, rmin=0, rmax=np.inf):
     integral = quad_vec(_AU_integrand, rmin, rmax,
-                        args=(k, dwf, AN),
+                        args=(k, dwf, nff),
                         workers=8
                         )[0]
     return integral * 2
 
-def _AT(k, dwf, AN, rmin=0, rmax=np.inf):
+def _AT(k, dwf, nff, rmin=0, rmax=np.inf):
     k = regulate_zero(k) # avoid division by zero
     integral = quad_vec(_AT_integrand, rmin, rmax,
-                        args=(k, dwf, AN),
+                        args=(k, dwf, nff),
                         workers=8
                         )[0]
     return integral * 2
 
-def _DU(k, dwf, AN, JN, DN, rmin=0, rmax=np.inf):
+def _DU(k, dwf, nff, rmin=0, rmax=np.inf):
     k = regulate_zero(k) # avoid division by zero
     integral = quad_vec(_DU_integrand, rmin, rmax,
-                        args=(k, dwf, AN, JN, DN),
+                        args=(k, dwf, nff),
                         workers=8
                         )[0]
     return integral * 2
 
-def _DT1(k, dwf, AN, JN, DN, rmin=0, rmax=np.inf):
+def _DT1(k, dwf, nff, rmin=0, rmax=np.inf):
     k = regulate_zero(k) # avoid division by zero
     integral = quad_vec(_DT1_integrand, rmin, rmax,
-                        args=(k, dwf, AN, JN, DN),
+                        args=(k, dwf, nff),
                         workers=8
                         )[0]
     return integral * 2
 
-def _DT2(k, dwf, AN, JN, rmin=0, rmax=np.inf):
+def _DT2(k, dwf, nff, rmin=0, rmax=np.inf):
     k = regulate_zero(k) # avoid division by zero
     integral = quad_vec(_DT2_integrand, rmin, rmax,
-                        args=(k, dwf, AN, JN),
+                        args=(k, dwf, nff),
                         workers=8
                         )[0]
     return integral * 2
 
-def _cU(k, dwf, AN, cN, rmin=0, rmax=np.inf, formula='fast'):
+def _cU(k, dwf, nff, rmin=0, rmax=np.inf, formula='fast'):
     k = regulate_zero(k) # avoid division by zero
     if(formula=='fast'):
         integrand = _cU_integrand
@@ -396,12 +396,12 @@ def _cU(k, dwf, AN, cN, rmin=0, rmax=np.inf, formula='fast'):
     else:
         raise ValueError("{} is not a valid formula key.".format(formula))
     integral = quad_vec(integrand, rmin, np.inf,
-                        args=(k, dwf, AN, cN),
+                        args=(k, dwf, nff),
                         workers=8
                         )[0]
     return integral * 2
 
-def _cT1(k, dwf, AN, JN, cN, rmin=0, rmax=np.inf, formula='fast'):
+def _cT1(k, dwf, nff, rmin=0, rmax=np.inf, formula='fast'):
     k = regulate_zero(k) # avoid division by zero
     if(formula=='fast'):
         integrand = _cT1_integrand
@@ -410,12 +410,12 @@ def _cT1(k, dwf, AN, JN, cN, rmin=0, rmax=np.inf, formula='fast'):
     else:
         raise ValueError("{} is not a valid formula key.".format(formula))
     integral = quad_vec(integrand, rmin, np.inf,
-                        args=(k, dwf, AN, JN, cN),
+                        args=(k, dwf, nff),
                         workers=8
                         )[0]
     return integral * 2
 
-def _cT2(k, dwf, AN, JN, rmin=0, rmax=np.inf, formula='fast'):
+def _cT2(k, dwf, nff, rmin=0, rmax=np.inf, formula='fast'):
     k = regulate_zero(k) # avoid division by zero
     if(formula=='fast'):
         integrand = _cT2_integrand
@@ -424,31 +424,31 @@ def _cT2(k, dwf, AN, JN, rmin=0, rmax=np.inf, formula='fast'):
     else:
         raise ValueError("{} is not a valid formula key.".format(formula))
     integral = quad_vec(integrand, rmin, np.inf,
-                        args=(k, dwf, AN, JN),
+                        args=(k, dwf, nff),
                         workers=8
                         )[0]
     return integral * 2
 
-def _J(k, dwf, AN, JN, rmin=0, rmax=np.inf):
+def _J(k, dwf, nff, rmin=0, rmax=np.inf):
     k = regulate_zero(k) # avoid division by zero
     integral = quad_vec(_J_integrand, rmin, rmax,
-                        args=(k, dwf, AN, JN),
+                        args=(k, dwf, nff),
                         workers=8
                         )[0]
     return integral * 2
 
-def _S(k, dwf, SN, rmin=0, rmax=np.inf):
+def _S(k, dwf, nff, rmin=0, rmax=np.inf):
     k = regulate_zero(k) # avoid division by zero
     integral = quad_vec(_S_integrand, rmin, rmax,
-                        args=(k, dwf, SN),
+                        args=(k, dwf, nff),
                         workers=8
                         )[0]
     return integral * 2
 
-def _sbar(k, dwf, SN, rmin=0, rmax=np.inf):
+def _sbar(k, dwf, nff, rmin=0, rmax=np.inf):
     k = regulate_zero(k) # avoid division by zero
     integral = quad_vec(_sbar_integrand, rmin, np.inf,
-                        args=(k, dwf, SN),
+                        args=(k, dwf, nff),
                         workers=8
                         )[0]
     return integral * 2
--- deupack/emtff/nucleon/chooser.py
+++ deupack/emtff/nucleon/chooser.py
@@ -8,42 +8,19 @@ from .nff import NFF
 from .ba import nff_ba
 from .hz import nff_hz
 from .mab import nff_mab
-#from . import misc as misc_mod
 from .mit import nff_mit
 from .point import nff_point
 
-# Add mapping for convenient selection
-#NUCLEONFFS = {
-#        'mitq' : (
-#            mit_mod.AN_q, mit_mod.JN_q, mit_mod.DN_q, misc_mod.cN_q, misc_mod.SN_q
-#            ),
-#        'mitg' : (
-#            mit_mod.AN_g, mit_mod.JN_g, mit_mod.DN_g, misc_mod.cN_g, misc_mod.SN_g
-#            ),
-#        'hzq' : (
-#            mit_mod.AN_q, mit_mod.JN_q, hz_mod.DN_q, misc_mod.cN_q, misc_mod.SN_q
-#            ),
-#        'hzg' : (
-#            mit_mod.AN_g, mit_mod.JN_g, hz_mod.DN_g, misc_mod.cN_g, misc_mod.SN_g
-#            )
-#        }
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# TODO: quark-only and gluon-only choices
 
 def choose_nff(nff):
     ''' Return a tuple (AN,JN,DN,cbarN,SN) according to a choice
     of nucleon EMTFFs.
     '''
-    ## if isinstance(nff, str):
-    ##     key = nff.lower()
-    ##     if key in NUCLEONFFS:
-    ##         return NUCLEONFFS[key]
-    ##     raise ValueError(f"Unknown nff '{nff}', valid: {list(NUCLEONFFS.keys())}")
-    ## # module-like object with attributes u,w,u1,...
-    ## for attr in ('AN','JN','DN','cN','SN'):
-    ##     if not hasattr(nff, attr):
-    ##         raise ValueError("nff module must have attributes: AN,JN,DN,cN,SN")
-    ## return (nff.AN,nff.JN,nff.DN,nff.cN,nff.SN)
     if(isinstance(nff, NFF)):
-        return (nff.AN, nff.JN, nff.DN, nff.cN, nff.SN)
+        return nff
     elif(isinstance(nff, str)):
         if(nff=='ba'):
             _nff = nff_ba()
@@ -57,5 +34,5 @@ def choose_nff(nff):
             _nff = nff_point()
         else:
             raise ValueError("nff={} not recognized.".format(nff))
-        return (_nff.AN, _nff.JN, _nff.DN, _nff.cN, _nff.SN)
+        return _nff
     raise TypeError("nff should be NFF or str.")
