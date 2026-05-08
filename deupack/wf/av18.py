f39cdd767c62ebfa35e5d7f3ef814d7dc8998da6
Adam Freese <adamfreese@proton.me>
2026-05-08 14:53:01 -0400

    Fix bug in AV18 D-wave only
    An extra __init__ was overriding the name string identifier

--- deupack/wf/av18.py
+++ deupack/wf/av18.py
@@ -161,11 +161,6 @@ class dwf_av18_d_only(dwf_av18):
         self.name = "av18-d-only"
         return
 
-    def __init__(self):
-        super().__init__()
-        self._make_wf()
-        return
-
     def u(self, r):
         return 0
 
